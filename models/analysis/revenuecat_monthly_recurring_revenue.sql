758e533bded2e919ff45d009c10c74d0e1f4ff69
Jurri Gerretsen <62351599+jurrigerretsen@users.noreply.github.com>
2024-02-21 16:01:01 +0100

    ✨ Improve mrr accuracy

--- models/analysis/revenuecat_monthly_recurring_revenue.sql
+++ models/analysis/revenuecat_monthly_recurring_revenue.sql
@@ -1,8 +1,14 @@
 with
 
 subscription_transactions as (
-    select * from {{ ref('revenuecat_subscription_transactions') }} where valid_to is null
-),
+    select * from {{ ref('revenuecat_subscription_transactions') }} 
+    where 
+        valid_to is null 
+        and is_trial_period = false 
+        and ownership_type != 'FAMILY_SHARED' 
+        and datediff('s', start_time, expected_end_time)::float > 0 
+        and store != 'promotional'
+), 
 
 date_spine as (
     select distinct date_month from {{ ref('revenuecat_date_spine') }}
@@ -44,8 +50,13 @@ final as (
 
     left join
         transaction_monthly_revenue
-        on date_spine.date_month >= transaction_monthly_revenue.start_time::date
-        and last_day(date_spine.date_month, month) <= last_day(transaction_monthly_revenue.effective_end_time::date, month)
+        on transaction_monthly_revenue.start_time::date <= last_day(date_month, month)
+        and case 
+                when transaction_monthly_revenue.effective_end_time >= convert_timezone('America/Los_Angeles', 'UTC', SYSDATE()::date) -- Assumes that RevenueCat runs their data model at this time. This gives an 99.9% accurate MRR number for the current month & prior months
+                then transaction_monthly_revenue.effective_end_time::date > date_spine.date_month
+                else transaction_monthly_revenue.effective_end_time::date > last_day(date_spine.date_month, month)
+            end
+
 
     group by
         date_spine.date_month,
